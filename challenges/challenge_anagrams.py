a7d00998a1829318408e50fdbc49aea24ef7ac14
walacenascimento <wlcens@hotmail.com>
2022-10-17 18:17:43 -0300

    Implementa o arquivo challenge_anagrams, resolve o requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def aux(word, anagram):
+    for letter in word:
+        if letter in anagram:
+            anagram = anagram.replace(letter, "", 1)
+        else:
+            return False
+
+    return True
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
+    if len(first_string) != len(second_string):
+        return False
+    elif not first_string or not second_string:
+        return False
+
+    word, anagram = first_string.lower(), second_string.lower()
+    return aux(word, anagram)
