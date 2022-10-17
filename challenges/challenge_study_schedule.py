31f8dc812e9b9261fdad42e130a17eb77b4898f0
walacenascimento <wlcens@hotmail.com>
2022-10-17 14:41:35 -0300

    Implementando o arquivo challenge_study_schedule, resolvendo o requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,20 @@
+def verify(tuple_time, target_time):
+    if target_time in range(tuple_time[0], tuple_time[1] + 1):
+        return 1
+    else:
+        return 0
+
+
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    if target_time is None:
+        return None
+
+    counter = 0
+    for tuple_time in permanence_period:
+        if type(tuple_time[0]) is not int or type(tuple_time[1]) is not int:
+            return None
+
+        counter += verify(tuple_time, target_time)
+
+    return counter
